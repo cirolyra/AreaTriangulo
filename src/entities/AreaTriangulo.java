047938318d57c77f6b517937137aacc34b53d049
cirol <cirol@192.168.147.1>
2021-05-18 20:57:20 -0300

    Correção do area();

--- src/entities/AreaTriangulo.java
+++ src/entities/AreaTriangulo.java
@@ -7,7 +7,7 @@ public class AreaTriangulo {
 	public double c;
 
 	public double area() {
-		double p = a + b + c / 2;
+		double p = (a + b + c) / 2;
 		// double area = Math.sqrt(p*(p-a)*(p-b)*(p-c));
 		return Math.sqrt(p * (p - a) * (p - b) * (p - c));
 	}
